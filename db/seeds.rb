8dcef94a627bfcb9b4d8353238c837301a5b3a1b
chanmike92@gmail.com <chanmike92@gmail.com>
2018-03-16 14:04:08 -0400

    Seeded data files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,19 +8,74 @@
 
 User.destroy_all
 Server.destroy_all
+Serversubscription.destroy_all
+Channel.destroy_all
+Message.destroy_all
 
-User.create(username: 'demoUser', password: 'demoPassword', email: 'demoEmail@demo.com')
+User.create(username: 'demoUser', password: 'asdfasdf', email: 'demoEmail@demo.com')
+User.create(username: 'Lucio', password: 'asdfasdf', email: 'Lucio@demo.com')
+User.create(username: 'Tracer', password: 'asdfasdf', email: 'Tracer@demo.com')
+User.create(username: 'Reaper', password: 'asdfasdf', email: 'Reaper@demo.com')
+User.create(username: 'Mercy', password: 'asdfasdf', email: 'Mercy@demo.com')
+User.create(username: 'Genji', password: 'asdfasdf', email: 'Genji@demo.com')
+User.create(username: 'Hanzo', password: 'asdfasdf', email: 'Hanzo@demo.com')
 
-5.times do
-  User.create(username: Faker::Pokemon.unique.name,
-              password: 'asdf1234',
-              email: Faker::Internet.unique.email
-              )
+Server.create(name: 'Overwatch', owner_id: 1)
+Server.create(name: 'Pokemon', owner_id: 1)
+Server.create(name: 'Kingdom Hearts', owner_id: 2)
+Server.create(name: 'Assassin Creed', owner_id: 3)
 
-end
+Serversubscription.create(user_id: 1, server_id: 1)
+Serversubscription.create(user_id: 2, server_id: 1)
+Serversubscription.create(user_id: 3, server_id: 1)
+Serversubscription.create(user_id: 4, server_id: 1)
+Serversubscription.create(user_id: 5, server_id: 1)
+Serversubscription.create(user_id: 6, server_id: 1)
+Serversubscription.create(user_id: 7, server_id: 1)
+Serversubscription.create(user_id: 1, server_id: 2)
+Serversubscription.create(user_id: 2, server_id: 2)
+Serversubscription.create(user_id: 3, server_id: 2)
+Serversubscription.create(user_id: 4, server_id: 2)
+Serversubscription.create(user_id: 1, server_id: 3)
+Serversubscription.create(user_id: 1, server_id: 3)
+Serversubscription.create(user_id: 2, server_id: 3)
+Serversubscription.create(user_id: 4, server_id: 3)
+Serversubscription.create(user_id: 6, server_id: 3)
+Serversubscription.create(user_id: 7, server_id: 3)
+Serversubscription.create(user_id: 1, server_id: 4)
+Serversubscription.create(user_id: 3, server_id: 4)
+Serversubscription.create(user_id: 5, server_id: 4)
+Serversubscription.create(user_id: 6, server_id: 4)
+Serversubscription.create(user_id: 7, server_id: 4)
 
-5.times do
-  Server.create(name: Faker::Pokemon.unique.move,
-                owner_id: rand(1..30)
-                )
-end
+Channel.create(name: 'Blackwatch', server_id: 1)
+Channel.create(name: "Doomfist's Room", server_id: 1)
+Channel.create(name: 'Pallet Town', server_id: 2)
+Channel.create(name: "Viridian Forest", server_id: 2)
+Channel.create(name: 'Pewter City', server_id: 2)
+Channel.create(name: "Cinnabar Island", server_id: 2)
+Channel.create(name: 'Agrabah', server_id: 3)
+Channel.create(name: "Castle Oblivion", server_id: 3)
+Channel.create(name: 'Neverland', server_id: 3)
+Channel.create(name: "Destiny Island", server_id: 3)
+Channel.create(name: 'New York', server_id: 4)
+Channel.create(name: "Florence", server_id: 4)
+Channel.create(name: 'Grand Temple', server_id: 4)
+Channel.create(name: "Masyaf", server_id: 4)
+
+Message.create(body: "First Message", author_id: 1, channel_id: 1)
+Message.create(body: "First Message", author_id: 1, channel_id: 2)
+Message.create(body: "First Message", author_id: 1, channel_id: 3)
+Message.create(body: "First Message", author_id: 1, channel_id: 4)
+Message.create(body: "First Message", author_id: 1, channel_id: 5)
+Message.create(body: "First Message", author_id: 1, channel_id: 6)
+Message.create(body: "First Message", author_id: 1, channel_id: 7)
+Message.create(body: "First Message", author_id: 1, channel_id: 8)
+Message.create(body: "First Message", author_id: 1, channel_id: 9)
+Message.create(body: "First Message", author_id: 1, channel_id: 10)
+Message.create(body: "First Message", author_id: 1, channel_id: 11)
+Message.create(body: "First Message", author_id: 1, channel_id: 12)
+Message.create(body: "First Message", author_id: 1, channel_id: 13)
+Message.create(body: "First Message", author_id: 1, channel_id: 14)
+Message.create(body: "Second!", author_id: 2, channel_id: 1)
+Message.create(body: "Me third", author_id: 3, channel_id: 1)
