32a84936f1e4e4bbf09546f1fb9227e229fb5e58
Mike Chan <chanmike92@gmail.com>
2018-04-05 16:51:12 -0400

    Fix route upon login

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -11,7 +11,7 @@ const Auth = ({ component: Component, path, loggedIn, exact, currentUser }) => {
         !loggedIn ? (
           <Component {...props} />
         ) : (
-          <Redirect to={`/@me/server/`} />
+          <Redirect to={`/@me/`} />
         )}
     />
   );
