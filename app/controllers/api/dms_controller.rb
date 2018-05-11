e72bd3d3b7b213c719e66bf82022bb952a837646
Mike Chan <chanmike92@gmail.com>
2018-05-11 16:59:40 -0400

    test again

--- app/controllers/api/dms_controller.rb
+++ app/controllers/api/dms_controller.rb
@@ -1,64 +1,64 @@
 class Api::DmsController < ApplicationController
 
-  def index
-
-    @dms = current_user.dmchannels
-    if @channels
-      render 'api/channels/index'
-    else
-      render json: {}
-    end
-  end
-
-  def create
-    @channel = Channel.new(channel_params)
-
-    if @channel.save
-      Serverchannel.create(server_id: params[:id], channel_id: @channel.id)
-      render 'api/channels/show'
-    else
-      render json: @channel.errors.full_messages, status: 402
-    end
-  end
-
-
-  def show
-    @channel = Channel.find(params[:id])
-    @channel_messages = @channel.messages
-    if @channel
-      render 'api/channels/show'
-    else
-      render json: {errors: ['Channel does not exist']}, status: 402
-    end
-  end
-
-  def update
-    @channel = Channel.find(params[:id])
-    if @channel.update(channel_params)
-      render 'api/channels/show'
-    else
-      render json: @channel.errors.full_messages, status: 402
-    end
-  end
-
-  def destroy
-    @channel = Channel.find(params[:id])
-
-    if @channel
-      if @channel.server.owner_id == current_user.id
-        # @channels = @channel.server.channels
-        @channel.destroy!
-        render json: {}
-      else
-        render json: ['You do not have access'], status: 404
-      end
-    else
-      render json: ['Channel does not exist'], status: 404
-    end
-  end
-
-  private
-  def channel_params
-    params.require(:channel).permit(:name)
-  end
+  # def index
+  #
+  #   @dms = current_user.dmchannels
+  #   if @channels
+  #     render 'api/channels/index'
+  #   else
+  #     render json: {}
+  #   end
+  # end
+  #
+  # def create
+  #   @channel = Channel.new(channel_params)
+  #
+  #   if @channel.save
+  #     Serverchannel.create(server_id: params[:id], channel_id: @channel.id)
+  #     render 'api/channels/show'
+  #   else
+  #     render json: @channel.errors.full_messages, status: 402
+  #   end
+  # end
+  #
+  #
+  # def show
+  #   @channel = Channel.find(params[:id])
+  #   @channel_messages = @channel.messages
+  #   if @channel
+  #     render 'api/channels/show'
+  #   else
+  #     render json: {errors: ['Channel does not exist']}, status: 402
+  #   end
+  # end
+  #
+  # def update
+  #   @channel = Channel.find(params[:id])
+  #   if @channel.update(channel_params)
+  #     render 'api/channels/show'
+  #   else
+  #     render json: @channel.errors.full_messages, status: 402
+  #   end
+  # end
+  #
+  # def destroy
+  #   @channel = Channel.find(params[:id])
+  #
+  #   if @channel
+  #     if @channel.server.owner_id == current_user.id
+  #       # @channels = @channel.server.channels
+  #       @channel.destroy!
+  #       render json: {}
+  #     else
+  #       render json: ['You do not have access'], status: 404
+  #     end
+  #   else
+  #     render json: ['Channel does not exist'], status: 404
+  #   end
+  # end
+  #
+  # private
+  # def channel_params
+  #   params.require(:channel).permit(:name)
+  # end
 end
