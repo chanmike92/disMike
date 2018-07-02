b63872711d4a7005d42313980c43161614a36cb4
Mike Chan <chanmike92@gmail.com>
2018-07-01 23:16:38 -0400

    Created channels for dms and appearance

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -5,7 +5,7 @@ class ChatChannel < ApplicationCable::Channel
     # else
     #   channel = DmChannel.find(params[:id])
     # end
-    channel = params[:type] === "Channel" ? Channel.find(params[:id]) : DmChannel.find(params[:id])
+    channel = Channel.find(params[:id])
     stream_for channel
   end
 
