50bc0a1f54cb7104753f519cafa0ab1fe5be6f07
Mike Chan <chanmike92@gmail.com>
2018-08-15 19:40:18 -0400

    Remove sidekiq

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Converse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.active_job.queue_adapter = :sidekiq
 
     config.paperclip_defaults = {
       :storage => :s3,
